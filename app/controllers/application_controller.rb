280dba2a924e86c9b236fb64c3748e53f6df96a2
Daniel Sun <danielsun12345@gmail.com>
2020-04-18 02:20:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   
     post '/teams' do
       @team = Team.new(params[:team])
-      params[:team][:members].each do |hero|
+      params[:team][:members].each do |param|
         Hero.new(hero)
       end
       @heroes = Hero.all
