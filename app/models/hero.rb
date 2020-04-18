cd8735ca756ecb53e9496cdddaaed36a25bd62cf
Daniel Sun <danielsun12345@gmail.com>
2020-04-18 02:26:05 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,24 +1,16 @@
 class Hero
-
   attr_accessor :name, :power, :bio
-  
-  Heroes = []
-  
-  
-  def initialize(params)
-    @name = params[:name]
-    @power = params[:power]
-    @biography = params[:bio]
-    Heroes << self 
-  end 
 
-  def self.all 
-    Heroes
-  end 
-  
-  def self.clear 
-    Heroes.clear
-  end 
+  @@all = []
 
-  
-end 
+  def initialize(details)
+    @name = details[:name]
+    @power = details[:power]
+    @bio = details[:bio]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
